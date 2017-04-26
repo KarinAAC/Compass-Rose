7c75cfa5d11e64e1cb953d76d81d5345ede43e6b
Liliana Fiore <liliana.fiorela@gmail.com>
2017-04-26 12:33:52 -0500

    subiendo seccion vestuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,18 @@ function callModal() {
   })
 
 }
+
+/** Evento drag & drog**/
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+}
